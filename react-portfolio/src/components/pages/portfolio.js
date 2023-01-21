b94e5f59b0e0024962f49b820448d49b62f87b6b
Camryn Kelly <camrynkelly@Camryns-MacBook-Pro.local>
2023-01-20 17:41:10 -0800

    finsihed portfolio

--- react-portfolio/src/components/pages/portfolio.js
+++ react-portfolio/src/components/pages/portfolio.js
@@ -20,38 +20,38 @@ function portfolio(){
                     <div className="column" id="jate">
                     <ul>
                             <li>
-                                <h1>Mern Stack</h1>
+                                <h1>J.A.T.E</h1>
                             </li>
                             <li>
-                             <a href='https://github.com/ncortes04' className='gitButtonPortfolio'></a>
+                             <a href='https://github.com/ncortes04/Challenge-19-J.A.T.E' className='gitButtonPortfolio'></a>
                             </li>
                         </ul>
                     </div>
                     <div className="column" id="tictactoe"><ul>
                             <li>
-                                <h1>Mern Stack</h1>
+                                <h1>Tic Tac Toe</h1>
                             </li>
                             <li>
-                             <a href='https://github.com/ncortes04' className='gitButtonPortfolio'></a>
+                             <a href='https://github.com/ncortes04/Tic-Tac-Toe' className='gitButtonPortfolio'></a>
                             </li>
                         </ul></div>
                     <div className="column" id="datadeposits">
                     <ul>
                             <li>
-                                <h1>Mern Stack</h1>
+                                <h1>Data Deposits</h1>
                             </li>
                             <li>
-                             <a href='https://github.com/ncortes04' className='gitButtonPortfolio'></a>
+                             <a href='https://github.com/Hrzonca/data-deposits' className='gitButtonPortfolio'></a>
                             </li>
                         </ul>
                     </div>
                     <div className="column" id="weatherforcast">
                     <ul>
                             <li>
-                                <h1>Mern Stack</h1>
+                                <h1>Weather Forecast</h1>
                             </li>
                             <li>
-                             <a href='https://github.com/ncortes04' className='gitButtonPortfolio'></a>
+                             <a href='https://github.com/ncortes04/weather-forcast' className='gitButtonPortfolio'></a>
                             </li>
                         </ul>
                     </div>
